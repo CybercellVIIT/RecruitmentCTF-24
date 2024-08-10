a567aca12e9606516a9fcc6ec3d3f7184c31d5ea
SamarthGhante <samarthghante.dev@gmail.com>
2024-08-10 20:15:45 +0530

    added keygenme files | rev

---
+++ rev/keygenme/keygenme.py
@@ -0,0 +1,7 @@
+def check_input(data):
+	if(len(data) == 8):
+		if(data[3]=='d' and data[5]=='u' and data[6]=='c' and data[0]=='g' and data[1]=='0' and data[7]=='k' and data[2]=='0' and data[4]=='l'):
+			print('0'+'n'+'3'+'_'+'w'+'0'+'r'+'d'+'_'+'4'+'t'+'_'+'4'+'_'+'t'+'1'+'m'+'3')
+
+user_input = input("Enter the key: ")
+check_input(user_input)
